9052998e6436a8f11303699825d1be118294d339
Leo-Simpson <leo.bill.simpson@gmail.com>
2023-05-23 17:56:24 +0200

    cleaner way to define dynamics and example with random walk

--- si4kalman/__init__.py
+++ si4kalman/__init__.py
@@ -1,7 +1,2 @@
-from .modelexamples import (
-    minimal_model,
-    condconv_model,
-    rw_model
-)
 from .modelparser import ProblemParser, ModelParser
 from .visual import latexify, plot_data, plot_est, plot_res
