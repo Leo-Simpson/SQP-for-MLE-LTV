abf39a96de3e3d6f5cd2127cf9e3416e36ee9aac
Leo-Simpson <leo.bill.simpson@gmail.com>
2023-10-13 13:25:31 +0200

    change the structure, to allow several data series

--- src/kalmanSQP.py
+++ src/kalmanSQP.py
@@ -13,7 +13,6 @@ default_opts = {"maxiter":50,
           "globalization.gamma":0.1,
           }
 
-
 def myprod1(dA, B):
     # computes the derivative of A(t) B
     # return np.einsum("uvw,vx->uxw", dA, B)
@@ -38,7 +37,9 @@ class OPTKF:
     def __init__(self, problem, formulation, eqconstr=True, verbose=True, rescale=True, opts={}):
         self.model = problem.model
         self.problem = problem
-        self.N = problem.N
+        self.L2pen = problem.L2pen
+        self.datas = [{"x0":problem.x0, "P0":problem.P0, "ys":problem.ys}]
+
         self.formulation = formulation
         self.verbose = verbose
         self.rescale = rescale
@@ -51,7 +52,7 @@ class OPTKF:
         
         self.make_constr(eqconstr=eqconstr)
         # prepare linearizing functions
-        self.A_fns, self.b_fns, self.C, self.dA_fns, self.db_fns = self.problem.gradient_dyna_fn()
+        self.A_fns, self.b_fns, self.C, self.dA_fns, self.db_fns = problem.gradient_dyna_fn()
         self.dQ_fn, self.dR_fn = self.model.gradient_covariances_fn()
         self.inds_tri_M = np.tri(self.model.ny, k=-1, dtype=bool) # used to be np.bool
         
@@ -62,7 +63,7 @@ class OPTKF:
     def rinit(self):
         fn_names = [
             "linearized_fn","kalman_simulate",
-            "prepare","cost_eval","cost_derivatives","QP",
+            "prepare","cost_eval","derivatives","QP",
             "get_AbC","get_dAb","get_dQR","total"
             ]
         self.rtimes = {key:0 for key in fn_names}
@@ -110,10 +111,6 @@ class OPTKF:
         # furthermore, in the case of no_u, bs is empty
         As = [A_fn(alpha).full() for A_fn in self.A_fns]
         bs = [ b_fn(alpha).full().squeeze() for b_fn in self.b_fns ]
-        if len(As) == 1:
-            As = As * self.N
-        if len(bs) == 1:
-            bs = bs * self.N
         self.rtimes["get_AbC"] += time() - t0
         self.ncall["get_AbC"] += 1
         return As, bs
@@ -126,11 +123,6 @@ class OPTKF:
                 0, 1)
             for dA_fn in self.dA_fns]
         dbs = [db_fn(alpha).full() for db_fn in self.db_fns]
-
-        if len(dAs) == 1:
-            dAs = dAs * self.N
-        if len(dbs) == 1:
-            dbs = dbs * self.N
         self.rtimes["get_dAb"] += time() - t0
         self.ncall["get_dAb"] += 1
         return dAs, dbs
@@ -145,53 +137,62 @@ class OPTKF:
         self.ncall["get_dQR"] += 1
         return dQ, dR
 
-    def kalman_simulate(self, alpha, beta):
+    # functions that look at only one data serie
+    def kalman_simulate(self, alpha, beta, data):
         t0 = time()
-        Ls = np.empty( (self.N, self.model.nx, self.model.ny) )
-        Ms = np.empty( (self.N, self.model.ny, self.model.ny) )
-        Ss = np.empty( (self.N, self.model.ny, self.model.ny) )
-        Ps = np.empty( (self.N+1, self.model.nx, self.model.nx) )
+        N = len(data["ys"])
+        Ls = np.empty( (N, self.model.nx, self.model.ny) )
+        Ms = np.empty( (N, self.model.ny, self.model.ny) )
+        Ss = np.empty( (N, self.model.ny, self.model.ny) )
+        Ps = np.empty( (N+1, self.model.nx, self.model.nx) )
         logdetS = 0.
-        es = np.empty( (self.N, self.model.ny) )
-        xs = np.empty( (self.N+1, self.model.nx) )
+        es = np.empty( (N, self.model.ny) )
+        xs = np.empty( (N+1, self.model.nx) )
         As, bs = self.get_AbC(alpha)
+        if len(As) == 1:
+            As = As * N
+            bs = bs * N
         Q, R = self.model.get_QR(beta)
-
-        xs[0] = self.problem.x0
-        Ps[0] = self.problem.P0
-        for k in range(self.N):
+        xs[0] = data["x0"]
+        Ps[0] = data["P0"]
+        for k in range(N):
             Ss[k], logdetSk, Ms[k], Ls[k], es[k], xs[k+1], Ps[k+1] = \
                 kalman_step(
                     As[k], bs[k], self.C, Q, R,
-                    Ps[k], xs[k], self.problem.ys[k],
+                    Ps[k], xs[k], data["ys"][k],
                     self.inds_tri_M)
 
             logdetS = logdetS + logdetSk
-        self.aux = {
+        aux = {
                "P":Ps, "L":Ls, "M":Ms, "S":Ss, "logdetS":logdetS,
                "A" : As, "b": bs,
                "e" : es, "x":xs
         }
         self.rtimes["kalman_simulate"] += time() - t0
         self.ncall["kalman_simulate"] += 1
+        return aux
 
-    def cost_derivatives(self, alpha, beta):
+    def derivatives(self, alpha, beta, aux):
         t0 = time()
         gradient, hessian = 0., 0.
         dx_dalpha, dP_dalpha = np.zeros((self.model.nx, self.model.nalpha)), np.zeros((self.model.nx, self.model.nx, self.model.nalpha))
         dx_dbeta, dP_dbeta = np.zeros((self.model.nx, self.model.nbeta)), np.zeros((self.model.nx, self.model.nx, self.model.nbeta))
         dAs, dbs = self.get_dAb(alpha)
         dQ, dR = self.get_dQR(beta)
-        for k in range(self.N):
-            e, M, S  = self.aux["e"][k], self.aux["M"][k], self.aux["S"][k]
+        N = len(aux["e"])
+        if len(dAs) == 1:
+            dAs = dAs * N
+            dbs = dbs * N
+        for k in range(N):
+            e, M, S  = aux["e"][k], aux["M"][k], aux["S"][k]
             dM_dalpha, de_dalpha, dP_dalpha, dx_dalpha = \
                 kalman_step_dalpha(dx_dalpha, dP_dalpha, dAs[k], dbs[k],
                                    e, M, S, self.C,
-                                   self.aux["x"][k], self.aux["L"][k], self.aux["P"][k], self.aux["A"][k])
+                                   aux["x"][k], aux["L"][k], aux["P"][k], aux["A"][k])
             dM_dbeta, de_dbeta, dP_dbeta, dx_dbeta = \
                 kalman_step_dbeta(dx_dbeta, dP_dbeta, dQ, dR, 
                                     e, M, S, self.C,
-                                    self.aux["x"][k], self.aux["L"][k], self.aux["P"][k], self.aux["A"][k])
+                                    aux["x"][k], aux["L"][k], aux["P"][k], aux["A"][k])
             
             de_dab = np.concatenate([de_dalpha, de_dbeta], axis=-1)
             dM_dab = np.concatenate([dM_dalpha, dM_dbeta], axis=-1)
@@ -206,25 +207,41 @@ class OPTKF:
                 hessian_k = 2 * np.linalg.multi_dot([ (dMe+Mde).T, S, dMe+Mde  ])
             gradient = gradient + gradient_k
             hessian = hessian + hessian_k
-        self.delete_unecessary()
-        self.rtimes["cost_derivatives"] += time() - t0
-        self.ncall["cost_derivatives"] += 1
+        delete_unecessary(aux)
+        self.rtimes["derivatives"] += time() - t0
+        self.ncall["derivatives"] += 1
         return gradient, hessian
-      
-    def delete_unecessary(self):
-        del self.aux["x"]
-        del self.aux["S"]
-        del self.aux["P"]
-        del self.aux["A"]
-        del self.aux["b"]
-        del self.aux["L"]
+    
+    def scale(self, aux):
+        dimension = len(aux["e"]) * self.model.ny
+        lamb = np.sum(aux["e"][:, np.newaxis] @ aux["M"] @ aux["e"][..., np.newaxis]) / dimension
+        aux["M"] = aux["M"] / lamb # TODO
+        aux["P"] = aux["P"] * lamb
+        aux["S"] = aux["S"] * lamb
+        aux["logdetS"] = aux["logdetS"] + dimension * np.log(lamb)
+        return lamb
 
+    # function that looks at all data series
+    def derivatives_all(self, alpha, beta, auxs):
+        gradient, hessian = 0., 0.
+        for aux in auxs:
+            gradient_i, hessian_i = self.derivatives(alpha, beta, aux)
+            gradient = gradient + gradient_i
+            hessian = hessian + hessian_i
+        return gradient, hessian
+    
     def cost(self, alpha, beta):
+        # everytime one computes the cost, one also computes the simulation
         t0 = time()
-        value = cost_eval(self.aux["M"], self.aux["e"], self.aux["logdetS"], alpha, beta, self.formulation, L2pen=self.problem.L2pen)
+        auxs, value = [], 0.
+        for data in self.datas:
+            aux_i = self.kalman_simulate(alpha, beta, data)
+            value_i = cost_eval(aux_i["M"], aux_i["e"], aux_i["logdetS"], alpha, beta, self.formulation, L2pen=self.L2pen)
+            auxs.append(aux_i)
+            value += value_i
         self.rtimes["cost_eval"] += time() - t0
         self.ncall["cost_eval"] += 1
-        return value
+        return auxs, value
     
     def eval_kkt_error(self, ab, gradient, multiplier):
         kkt_error =  gradient + self.derineqconstraints(ab, 0).full().T @ multiplier
@@ -252,55 +269,63 @@ class OPTKF:
         self.ncall["QP"] += 1
         return x, lam, der
     
-    def scale(self):
-        dimension = self.N * self.model.ny
-        lamb = np.sum(self.aux["e"][:, np.newaxis] @ self.aux["M"] @ self.aux["e"][..., np.newaxis]) / dimension
-        self.aux["M"] = self.aux["M"] / lamb # TODO
-        self.aux["P"] = self.aux["P"] * lamb
-        self.aux["S"] = self.aux["S"] * lamb
-        self.aux["logdetS"] = self.aux["logdetS"] + dimension * np.log(lamb)
-        return lamb
-
-    def stepSQP(self, alpha, beta, prev_cost):
-        gradient, hessian = self.cost_derivatives(alpha, beta)
+    def line_search(self, alpha0, beta0, alpha1, beta1, der, cost, maxiter, gamma, b):
+        tau = 1.
+        if der < 0.:
+            return None, 0., cost
+        for i_glob in range(maxiter):
+            alpha_middle = (1 - tau) * alpha0 + tau * alpha1
+            beta_middle = (1 - tau) * beta0 + tau * beta1
+            auxs_middle, cost_middle = self.cost(alpha_middle, beta_middle)
+            condition = (cost - cost_middle) / tau > gamma * der and np.all(beta_middle >= 0.)
+            if condition:
+                    if self.verbose: print(f" tau = {tau}")
+                    return auxs_middle, tau, cost_middle
+            tau = tau * b
+        if self.verbose: print("Globalization did not finish with tau = {}".format(tau))
+        return None, 0., cost
+    
+    def stepSQP(self, alpha, beta, prev_cost, auxs):
+        gradient, hessian = self.derivatives_all(alpha, beta, auxs)
         ab = np.concatenate([alpha, beta])
         linconstr, lin_eqconstr = self.make_lin_constr(ab)
-        Q, p = prepareQP(ab, gradient, hessian, pen_step=self.opts["pen_step"], L2reg=self.problem.L2pen)
+        Q, p = prepareQP(ab, gradient, hessian, pen_step=self.opts["pen_step"], L2reg=self.L2pen)
         ab_next, multiplier, der = self.solveQP(Q, p, linconstr, lin_eqconstr, ab)
         alpha_next, beta_next = ab_next[:self.nalpha], ab_next[self.nalpha:]
         kkt_error = self.eval_kkt_error(ab, gradient, multiplier)
-        termination = None
-        if der < 0.:
-            termination = f"non-descending direction : derivative={der:.2e}"
-        else:
-            tau, new_cost = self.line_search(alpha, beta, alpha_next, beta_next, der, prev_cost,
+        auxs, tau, new_cost = self.line_search(alpha, beta, alpha_next, beta_next, der, prev_cost,
                                             self.opts["globalization.maxiter"], self.opts["globalization.gamma"], self.opts["globalization.beta"])
-            if kkt_error < self.opts["tol.kkt"]:
-                termination = f"tol.kkt : kkt error= {kkt_error:.2e}"
-            elif der < self.opts["tol.direction"]:
-                termination = f"tol.direction, derivative={der:.2e}"
-            elif prev_cost - new_cost < max(abs(prev_cost),abs(new_cost)) * self.opts["rtol.cost_decrease"]:
-                termination = f"rtol.cost_decrease, cost decrease = {prev_cost - new_cost:.2e}"
-            elif tau is None:
-                termination = "globalization.maxiter"
-                tau = 0.
         alpha = (1 - tau) * alpha + tau * alpha_next
         beta = (1 - tau) * beta + tau * beta_next
-        if self.rescale:
-            beta = beta * self.scale()
-        return alpha, beta, new_cost, termination
+        if self.rescale and (auxs is not None):
+            beta = beta * np.mean([self.scale(aux) for aux in auxs])
+        termination = self.get_termination(prev_cost, (auxs is None), der, kkt_error, new_cost)
+        return alpha, beta, new_cost, auxs, termination
+
+    def get_termination(self, prev_cost, auxs_is_None, der, kkt_error, new_cost):
+        if auxs_is_None:
+            if der < 0.:
+                return f"non-descending direction : derivative={der:.2e}"
+            return "globalization.maxiter"     
+        elif kkt_error < self.opts["tol.kkt"]:
+            return f"tol.kkt : kkt error= {kkt_error:.2e}"
+        elif der < self.opts["tol.direction"]:
+            return f"tol.direction, derivative={der:.2e}"
+        elif prev_cost - new_cost < max(abs(prev_cost),abs(new_cost)) * self.opts["rtol.cost_decrease"]:
+            return f"rtol.cost_decrease, cost decrease = {prev_cost - new_cost:.2e}"
+        else:
+            return None
 
     def SQP_kalman(self, alpha0, beta0):
         t0 = time()
         alphaj = alpha0.copy()
         betaj = beta0.copy()
-        self.kalman_simulate(alphaj, betaj)
-        cost = self.cost(alphaj, betaj)
+        auxs, cost = self.cost(alphaj, betaj)
         if self.rescale:
-            betaj = betaj * self.scale()
+            betaj = betaj * np.mean([self.scale(aux) for aux in auxs])
         termination = "maxiter"
         for j in range(self.opts["maxiter"]):
-            alphaj, betaj, cost, termination  = self.stepSQP(alphaj, betaj, cost)
+            alphaj, betaj, cost, auxs, termination  = self.stepSQP(alphaj, betaj, cost, auxs)
             if self.verbose:
                 print(f"Iteration {j+1}, Current cost {cost:2e}")
             if termination is not None:
@@ -313,41 +338,15 @@ class OPTKF:
         stats["return_status"] = stats["termination"]
         return alphaj, betaj, stats
 
-    def line_search(self, alpha0, beta0, alpha1, beta1, der, cost, maxiter, gamma, b):
-        tau = 1.
-        for i_glob in range(maxiter):
-            alpha_middle = (1 - tau) * alpha0 + tau * alpha1
-            beta_middle = (1 - tau) * beta0 + tau * beta1
-            self.kalman_simulate(alpha_middle, beta_middle)
-            cost_middle = self.cost(alpha_middle, beta_middle)
-            condition = (cost - cost_middle) / tau > gamma * der and np.all(beta_middle >= 0.)
-            if condition:
-                    if self.verbose: print(f" tau = {tau}")
-                    return tau, cost_middle
-            tau = tau * b
-        if self.verbose: print("Globalization did not finish with tau = {}".format(tau))
-        return None, cost
-
-    # def cost_derivatives_fd(self, alpha, beta, formulation, dalpha, dbeta):
-    #     eps = 1e-5
-    #     _, cost0 = self.cost(alpha, beta, formulation)
-    #     _, cost_plus = self.cost(alpha+eps*dalpha, beta+eps*dbeta, formulation)
-    #     der = (cost_plus - cost0)/eps
-    #     return der
-    # def verif(self, gradient, alpha, beta, formulation):
-    #     dalpha = np.random.random(self.model.nalpha)
-    #     dbeta = np.random.random(self.model.nbeta)
-
-    #     ad_alpha = gradient[:self.model.nalpha] @ dalpha
-    #     fd_alpha = self.cost_derivatives_fd(alpha, beta, formulation, dalpha, 0.)
-        
-    #     ad_beta = gradient[self.model.nalpha:] @ dbeta
-    #     fd_beta = self.cost_derivatives_fd(alpha, beta, formulation, 0., dbeta)
-        
-    #     print(f"verif dalpha : fd = {fd_alpha:.2e}, ad = {ad_alpha:.2e}, dif = {(fd_alpha-ad_alpha):.2e}")
-    #     print(f"verif dbeta : fd = {fd_beta:.2e}, ad = {ad_beta:.2e}, dif = {(fd_beta-ad_beta):.2e}")
-
 # utils
+def delete_unecessary(aux):
+    del aux["x"]
+    del aux["S"]
+    del aux["P"]
+    del aux["A"]
+    del aux["b"]
+    del aux["L"]
+
 def cost_eval(Ms, es, logdet, alpha, beta, formulation, L2pen=0.):
     if formulation=="MLE":
         value1 = np.sum(es[:, np.newaxis] @ Ms @ es[..., np.newaxis])
